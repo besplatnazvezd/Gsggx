804e4d2a0f558e1795da20300b86c6f074fcb47a
besplatnazvezd <safery707@gmail.com>
2026-07-12 20:38:32 +0300

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-import os
-# Токен берем из переменных окружения (для Railway)
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8651401119:AAHXA_6eLh9ElicvrM5gXI2JIDcrbxJQsEc")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "7727345054")) # Ваш ID, чтобы получать чеки
